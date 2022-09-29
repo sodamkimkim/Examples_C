711d58815637acf0306a6118893e56574beb8100
sodamkimkim <theka265@gmail.com>
2022-09-29 14:11:20 +0900

    20220929 Function

---
+++ 20220929_Function/main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+// 함수 선언(function declaration)
+// 반환형 함수명(매개변수)
+// return-type functino-name(parameter)
+// 함수 정의(function definition)
+//int add3(int _val); // 함수 선언
+//void printhelloworld(void);
+//
+//int add3(int _val) // 정의
+//{
+//	int tmp = _val * 2;
+//	tmp /= 3;
+//	tmp += 10;
+//	return tmp;
+//	return _val + 3;
+//}
+//int main() {
+//	 함수(function)
+//	 함수 호출(function call)
+//	 매개변수 - 인자 : parameter - argument
+//	printf("add3(10) : %d\n", add3(10));
+//	return 0;
+//}
+//void ptinthelloword(void) {
+//	printf("hello, world!\n");
+//}
+
+int main() {
+	int i = 1;
+	int n = 5;
+	for (i = 1; i <= n;i++) {
+		
+		if (2*i-1 == ) { // 2*i-1이랑 (i마다 정해진값생김)
+			printf("**\n");
+			i++;
+		}
+		else {
+			printf("*");
+		}
+
+	}
+	return 0;
+}
