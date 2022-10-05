d2feec0cf11161ab947d8664952a67231b8af279
sodamkimkim <theka265@gmail.com>
2022-10-05 09:38:52 +0900

    function

--- 20220929_Function/main.c
+++ 20220929_Function/main.c
@@ -1,44 +1,28 @@
 #include <stdio.h>
-
-// 함수 선언(function declaration)
+// 함수 선언(Function Declaration)
 // 반환형 함수명(매개변수)
-// return-type functino-name(parameter)
-// 함수 정의(function definition)
-//int add3(int _val); // 함수 선언
-//void printhelloworld(void);
-//
-//int add3(int _val) // 정의
-//{
-//	int tmp = _val * 2;
-//	tmp /= 3;
-//	tmp += 10;
-//	return tmp;
-//	return _val + 3;
-//}
-//int main() {
-//	 함수(function)
-//	 함수 호출(function call)
-//	 매개변수 - 인자 : parameter - argument
-//	printf("add3(10) : %d\n", add3(10));
-//	return 0;
-//}
-//void ptinthelloword(void) {
-//	printf("hello, world!\n");
-//}
+// Return-Type Function-Name(Parameter)
+// 함수 정의(Function Definition)
+int Add3(int _val); // 함수 선언
+void PrintHelloWorld(void);
 
+int Add3(int _val) // 정의
+{
+	// int tmp = _val *2;
+	// tmp /= 3;
+	// tmp += 10;
+	// return tmp;
+	return _val + 3;
+
+}
 int main() {
-	int i = 1;
-	int n = 5;
-	for (i = 1; i <= n;i++) {
-		
-		if (2*i-1 == ) { // 2*i-1이랑 (i마다 정해진값생김)
-			printf("**\n");
-			i++;
-		}
-		else {
-			printf("*");
-		}
-
-	}
+	// 함수(Funciton)
+	// 함수 호출(Function Call)
+	// 매개변수 - 인자 : Parameter - Argument
+	printf("Add3(10) : %d\n", Add3(10));
 	return 0;
+
+}
+void PrintHelloWorld(void) {
+	printf("Hello, World\n");
 }
