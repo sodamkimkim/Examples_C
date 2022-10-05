75d7138c9b46fd30dc2c9ce15bc5e18094954b5c
sodamkimkim <theka265@gmail.com>
2022-10-05 16:05:20 +0900

    while pyramid

--- pyramid/main.c
+++ pyramid/main.c
@@ -14,8 +14,6 @@ int main()
 		if (star <= space) // 공백 출력 조건
 		{
 			printf(" ");
-			printf("star: %d\n", star);
-			printf("space: %d\n", space);
 			star++;
 		}
 		if (star >= space) // * 출력 조건 -> step수만큼의 반복 중에서 star가 계속 증가하다가 star>=space되는 지점에서 *출력
@@ -27,19 +25,26 @@ int main()
 		if (star - space > i) // 줄바꿈 조건
 		{
 			// space나 * 찍고나서 star - space > i 검사해서
-			// left가 클 때만 \n 출력한다.
-			// 즉 피라미드 마지막 줄에서는 \n출력 안한다.
+			// left가 클 때 \n 출력한다.
+			// 종료 조건 if문 실행되기 전까진 다 출력된다고 보면 된다.
 			printf("\n");
-			//printf("============\n");
-			//printf("star - space: %d\n", star - space);
-			//printf("i: %d\n", i);
+			printf("star: %d\n", star);
+			printf("space: %d\n", space);
+			printf("star - space: %d\n", star - space);
+			printf("i: %d\n", i);
+			printf("============\n");
 			i++;
-			space--; // 줄 바꿀 때 space --해준다. => 그 다음 줄은 공백이 1개씩 줄어들게 된다.
+			space--; // 줄 바꿀 때 space-- 해준다. => 그 다음 줄은 공백이 1개씩 줄어들게 된다. & step수에서 -- 된다.
+			//space--되다가 space == 0되어버리면 다음의 프로그램 종료 if문을 타게되면서 끝난다.
 			star = 0; 
 		}
 		if (space <= 0) // 프로그램 종료 조건
 		{
 			printf("나간다요");
+			printf("star: %d\n", star);
+			printf("space: %d\n", space);
+			printf("star - space: %d\n", star - space);
+			printf("i: %d\n", i);
 			break;
 		}
 	}
